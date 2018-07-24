ac57ebc7d6d843ac4ba4f066c5f261b46f311468
haneybot123 <40692777+haneybot123@users.noreply.github.com>
2018-07-24 12:14:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,8 +92,9 @@ client.on('message', async msg => { // eslint-disable-line
 					const embed1 = new Discord.RichEmbed()
 			        .setDescription(`**اختار رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
-					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					.setFooter(copy, client.user.avatarURL);
+					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
+					
 					// eslint-disable-next-line max-depth
 					try {
 						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
